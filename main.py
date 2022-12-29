3cbab44dcdb65e3b635939d82afa0adbcf8537f5
Sri Santhosh Reddy Medapati <86189839+srisanthoshreddy-medapati@users.noreply.github.com>
2022-12-30 00:19:40 +0530

    Update main.py
    Signed-off-by: Sri Santhosh Reddy Medapati <86189839+srisanthoshreddy-medapati@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
 from uniconnapps import connector
 
 #Replace with your config
@@ -9,20 +11,21 @@ uca_client = connector.UcaClient(
   )
 
 @uca_client.action
-def addition(a:float, b: float) -> float:
-  return a + b
+def addition(number1:float, number2: float) -> float:
+    return number1 + number2
 
 @uca_client.action
-def subtraction(a:float, b: float) -> float:
-  return a - b
+def subtraction(number1:float, number2: float) -> float:
+    return number1 - number2
 
 @uca_client.action
-def multiplication(a:float, b: float) -> float:
-  return a * b
+def multiplication(number1:float, number2: float) -> float:
+    return number1 * number2
 
 @uca_client.action
-def division(a:float, b: float) -> float:
-  return a / b
+def division(number1:float, number2: float) -> float:
+    return number1 / number2
 
 if __name__ == '__main__':
-  uca_client.run_forever()
+    uca_client.run_forever()
+    
